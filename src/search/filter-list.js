b4d95c22ee5c8b46c69ab423b12d55fbfea69000
jefflomacy <57078432+jefflomacy@users.noreply.github.com>
2019-11-12 19:27:25 -0500

    Building appliedFilters correctly now

--- src/search/filter-list.js
+++ src/search/filter-list.js
@@ -10,6 +10,10 @@ export default class FilterList extends React.Component {
      */
     constructor(props) {
         super(props);
+
+        this.state = {};
+        this.state.availableFilters = this.props.availableFilters;
+        this.state.appliedFilters = this.props.appliedFilters;
     }
 
     /**
@@ -19,17 +23,18 @@ export default class FilterList extends React.Component {
     render() {
         const filters = [];
         let counter = 0; // for unique ID for each form element.
-        for (let filterId of Object.keys(this.props.availableFilters)) {
-            let filter = this.props.availableFilters[filterId];
+        for (let filterId of Object.keys(this.state.availableFilters)) {
+            let filter = this.state.availableFilters[filterId];
             const valueOptions = [];
             for (let valueId of Object.keys(filter.values)) {
                 let label = filter.values[valueId];
                 const isApplied = this.isFilterApplied(filterId, valueId);
                 valueOptions.push((
-                    <div className="form-check">
+                    <div className="form-check" key={filterId + '-' + counter}>
                         <input className="form-check-input" type="checkbox"
                                data-filter-id={filterId} value={valueId} id={filterId + '-' + counter}
-                               checked={isApplied}/>
+                               onChange={this.toggleFilterValue.bind(this, filterId, valueId)}
+                               checked={isApplied} />
                         <label className="form-check-label" htmlFor={filterId + '-' + counter}>
                             {label}
                         </label>
@@ -38,7 +43,7 @@ export default class FilterList extends React.Component {
                 counter++;
             }
             filters.push((
-                <div className="mb-3">
+                <div className="mb-3" key={filterId}>
                     <div className="mb-1 font-weight-bold">{filter.name}</div>
                     {valueOptions}
                 </div>
@@ -47,13 +52,48 @@ export default class FilterList extends React.Component {
         return filters;
     }
 
+    /**
+     *
+     * @param filterId
+     * @param valueId
+     * @returns {*|boolean|boolean}
+     */
     isFilterApplied(filterId, valueId) {
-        if (this.props.appliedFilters) {
-            if (this.props.appliedFilters[filterId]) {
-                return this.props.appliedFilters[filterId].includes(valueId);
+        if (this.state.appliedFilters) {
+            if (this.state.appliedFilters[filterId]) {
+                return this.state.appliedFilters[filterId].includes(valueId);
             }
         }
 
         return false;
+    }
+
+    /**
+     * Toggle the given filter. Then, call the parent to let it know we changed.
+     *
+     * @param filterId
+     * @param valueId
+     * @param e
+     */
+    toggleFilterValue(filterId, valueId, e) {
+        const appliedFilters = this.state.appliedFilters;
+
+        if (appliedFilters[filterId] && appliedFilters[filterId].includes(valueId)) {
+            // If already applied, remove it.
+            appliedFilters[filterId] = appliedFilters[filterId].filter((v) => { return v !== valueId; });
+        } else {
+            // If not applied, add it.
+            if (!appliedFilters[filterId]) {
+                // Create the filter entry if not existing.
+                appliedFilters[filterId] = [];
+            }
+            appliedFilters[filterId].push(valueId);
+        }
+
+        this.setState({
+            appliedFilters: appliedFilters
+        });
+
+        this.props.onFilterChange(appliedFilters);
     }
 }
