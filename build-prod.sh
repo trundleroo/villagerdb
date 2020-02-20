ffa6d4c3571673aaaa495059549dd8b9d2b5e705
jefflomacy <57078432+jefflomacy@users.noreply.github.com>
2020-02-20 16:54:51 -0500

    prevent race condition on birthdays config during startup by people accessing the site before redis is built

--- build-prod.sh
+++ build-prod.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
-git pull origin master && 
+git pull origin master &&
+sudo docker stop villagerdb_webserver &&
 sudo docker stop villagerdb_app &&
 sudo docker-compose -f docker-compose-prod.yaml up --no-deps -d --build villagerdb_app &&
 sudo docker exec villagerdb_app bin/util build-redis-db && 
 sudo docker exec villagerdb_app bin/util delete-search-index && 
-sudo docker exec villagerdb_app bin/util build-search-index
+sudo docker exec villagerdb_app bin/util build-search-index &&
+sudo docker-compose -f docker-compose-prod.yaml up --no-deps -d villagerdb_webserver
