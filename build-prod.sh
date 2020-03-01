95f5f4a9520d72a27d0bd00a6f2f5a06bd632a9e
jefflomacy <57078432+jefflomacy@users.noreply.github.com>
2020-03-01 16:28:19 -0500

    fix docker permissions

--- build-prod.sh
+++ build-prod.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 git pull origin master &&
-sudo docker stop villagerdb_webserver &&
-sudo docker stop villagerdb_app &&
-sudo docker-compose -f docker-compose-prod.yaml up --no-deps -d --build villagerdb_app &&
-sudo docker exec villagerdb_app bin/util build-redis-db && 
-sudo docker exec villagerdb_app bin/util delete-search-index && 
-sudo docker exec villagerdb_app bin/util build-search-index &&
-sudo docker-compose -f docker-compose-prod.yaml up --no-deps -d villagerdb_webserver
+docker stop villagerdb_webserver &&
+docker stop villagerdb_app &&
+docker-compose -f docker-compose-prod.yaml up --no-deps -d --build villagerdb_app &&
+docker exec villagerdb_app bin/util build-redis-db && 
+docker exec villagerdb_app bin/util delete-search-index && 
+docker exec villagerdb_app bin/util build-search-index &&
+docker-compose -f docker-compose-prod.yaml up --no-deps -d villagerdb_webserver
