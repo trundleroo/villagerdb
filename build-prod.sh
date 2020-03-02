0fc22b4844672030f279b478e73553ba63bc2d7b
jefflomacy <57078432+jefflomacy@users.noreply.github.com>
2020-03-02 16:45:22 -0500

    Some build script updates and corrected mount point

--- build-prod.sh
+++ build-prod.sh
@@ -3,6 +3,8 @@ git pull origin master &&
 docker stop villagerdb_webserver &&
 docker stop villagerdb_app &&
 docker-compose -f docker-compose-prod.yaml up --no-deps -d --build villagerdb_app &&
+docker exec villagerdb_app npm run build-js &&
+docker exec villagerdb_app npm run build-css &&
 docker exec villagerdb_app bin/util build-redis-db && 
 docker exec villagerdb_app bin/util delete-search-index && 
 docker exec villagerdb_app bin/util build-search-index &&
