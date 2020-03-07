ddc53a8f29eb056fd3adf0e9eed3849f00dc7fdc
jefflomacy <57078432+jefflomacy@users.noreply.github.com>
2020-03-06 19:07:19 -0500

    hotfix autocomplete filling with garbage

--- src/search/autocomplete.js
+++ src/search/autocomplete.js
@@ -1,6 +1,11 @@
 import $ from "jquery";
 import _ from 'underscore';
 
+/**
+ * The currently-executing request. We cancel it if concurrent ones are happening.
+ */
+let currentRequest;
+
 $(document).ready(() => {
     /**
      * Make the list invisible.
@@ -14,27 +19,48 @@ $(document).ready(() => {
      * @param e
      */
     const fillAutoComplete = (e) => {
+        // Hide the list until we have results.
         const dataList = $('#autocomplete-items');
-        dataList.empty();
+        dataList.hide();
+
+        // Only continue if we have something to search for.
         const q = $(e.target).val().trim();
         if (q.length === 0) {
             return;
         }
 
-        $.ajax({
-            url: '/autocomplete?q=' + q,
-            type: 'GET',
-            dataType: 'json',
-            success: (suggestions) => {
-                dataList.show();
-                for (let s of suggestions) {
-                    const elem = $('<li></li>')
-                        .text(s)
-                        .on('click', doAutoComplete);
-                    dataList.append(elem);
-                }
+        // There is no way to ensure that a race condition won't cause 'currentRequest' to be null one in a million
+        // times, so we have to wrap this all in a try-catch block so we don't crash the browser if that ever happens.
+        try {
+            // Make the request, aborting the existing one if set.
+            if (currentRequest) {
+                currentRequest.abort();
             }
-        });
+            currentRequest = $.ajax({
+                url: '/autocomplete?q=' + q,
+                type: 'GET',
+                dataType: 'json',
+                success: (suggestions) => {
+                    // Empty the list before filling it.
+                    dataList.empty();
+                    for (let s of suggestions) {
+                        const elem = $('<li></li>')
+                            .text(s)
+                            .on('click', doAutoComplete);
+                        dataList.append(elem);
+                    }
+
+                    // Show it once filled.
+                    dataList.show();
+                    currentRequest = undefined;
+                },
+                error: function() {
+                    currentRequest = undefined;
+                }
+            });
+        } catch (e) {
+            console.error(e);
+        }
     };
 
     /**
